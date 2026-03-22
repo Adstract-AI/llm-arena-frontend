f4ab8b476f8580f9405812d04d82c022ff754017
Andrea-444 <andrea.stevanoska2@gmail.com>
2026-03-22 23:12:25 +0100

    bettered navbar

--- src/app/layout/AppLayout.tsx
+++ src/app/layout/AppLayout.tsx
@@ -4,7 +4,6 @@ import LightModeRoundedIcon from '@mui/icons-material/LightModeRounded'
 import { Link, NavLink, Outlet } from 'react-router-dom'
 
 const navItems = [
-  { to: '/', label: 'Launchpad' },
   { to: '/chatvote', label: 'ChatVote' },
   { to: '/leaderboard', label: 'Leaderboard' },
   { to: '/about', label: 'About' },
